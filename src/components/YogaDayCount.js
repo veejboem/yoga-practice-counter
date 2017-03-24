a2c94c6d250d7bac4ebc0d3a80c9e4453359debe
Valerie Boem <vboem@gmail.com>
2017-03-24 17:55:37 -0400

    Added icons

--- src/components/YogaDayCount.js
+++ src/components/YogaDayCount.js
@@ -1,4 +1,8 @@
 import '../stylesheets/ui.scss'
+import DirectionsWalk from 'react-icons/lib/md/directions-walk'
+import AvTimer from 'react-icons/lib/md/av-timer'
+import CalendarO from 'react-icons/lib/fa/calendar-o'
+import CheckCircleO from 'react-icons/lib/fa/check-circle-o'
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%')
@@ -11,15 +15,21 @@ const calcGoalProgress = (total, goal) => {
 export const YogaDayCount = ({total, classStyle, classDuration, goal}) => (
 			<div className="yoga-day-count">
 				<div className="total-days">
-					<span>{total} Total Days of Yoga Practice</span>
+					<span>
+            <CalendarO />
+            {total} Total Days of Yoga Practice
+          </span>
 				</div>
 				<div className="class-style">
+          <DirectionsWalk />
           <span>Style: {classStyle}</span>
 				</div>
 				<div className="class-duration">
+          <AvTimer />
           <span>{classDuration} Minutes</span>
 				</div>
         <div>
+          <CheckCircleO />
           <span>
             Goal Progress: {calcGoalProgress(
               total,
